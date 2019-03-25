686225e466d094901523e210c031ec60be8e4b74
gautambitious <mvgt.gautam@gmail.com>
2019-03-25 14:14:31 +0530

    ok

--- src/newSort.java
+++ src/newSort.java
@@ -3,24 +3,22 @@ public class newSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		final int[] arr = { 5, 4, 3, 2, 1 }; // This is my array
-	    int min;
-	    for (int i = 0; i < arr.length; i++) {
-	        // Assume first element is min
-	        min = i;
-	        for (int j = i + 1; j < arr.length; j++) {
-	            if (arr[j] < arr[min]) {
-	                min = j;
+		 int[] arr = {5,4,3,2,1}; // This is my array
+		    int min = 0;
 
-	            }
-	        }
-	        if (min != i) {
-	            final int temp = arr[i];
-	            arr[i] = arr[min];
-	            arr[min] = temp;
-	        }
-	        System.out.println(arr[i]);// I print the in ascending order
-	    }
-	}
+		    for(int i = 0;i<arr.length;i++)
+		    {
+		        //Assume first element is min
+		        min = i;
+		        for(int j = i + 1;j<arr.length;j++)
+		        {
+		            if(arr[j] < arr[min]) { min = j;}
+		        }
+		        int temp = arr[i];
+		        arr[i] = arr[min];
+		        arr[min] = temp;
+		        System.out.println(arr[i]);//I print the in ascending order 
+		    }
+		}
 
 }
