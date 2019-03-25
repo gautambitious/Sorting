713fbc1780c36419b894ce5c18dc283113c963f1
gautambitious <mvgt.gautam@gmail.com>
2019-03-25 14:16:36 +0530

    fdf

--- src/newSort.java
+++ src/newSort.java
@@ -3,23 +3,21 @@ public class newSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		final int[] arr = { 5, 4, 3, 2, 1 }; // This is my array
-	    int min;
-	    for (int i = 0; i < arr.length; i++) {
-	        // Assume first element is min
-	        min = i;
-	        for (int j = i + 1; j < arr.length; j++) {
-	            if (arr[j] < arr[min]) {
-	                min = j;
+		int [] arr = {5,4,3,2,1}; // This is my array
+	    int min = 0;
 
-	            }
+	    for(int i = 0;i<arr.length;i++)
+	    {
+	        //Assume first element is min
+	        min = i;//Selection sort algorithm says that find the minimum in the
+	                // array, but first element is not minimum.What's point here?
+	        for(int j = i + 1;j<arr.length;j++)
+	        {
+	            int temp = arr[i];
+	            arr[i] = arr[j];
+	            arr[j] = temp;
+	            System.out.println(arr[i]);//I print the in ascending order 
 	        }
-	        if (min != i) {
-	            final int temp = arr[i];
-	            arr[i] = arr[min];
-	            arr[min] = temp;
-	        }
-	        System.out.println(arr[i]);// I print the in ascending order
 	    }
 	}
 
